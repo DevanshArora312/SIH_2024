12e8d84e402e5bfa7efedcd92662d48f47f61d73
aryan <aryansatija2003@gmail.com>
2024-09-23 21:59:34 +0530

    proxy server complete

--- wallet-extension/wallet-server/index.js
+++ wallet-extension/wallet-server/index.js
@@ -4,7 +4,39 @@ const Moralis = require("moralis").default;
 require("dotenv").config();
 const app = express();
 app.use(cors());
-const port = process.env.PORT
+const port = process.env.PORT;
+
+app.get("/getFunds", async(req, res)=>{
+    const { userAddress, chain } = req.query;
+    
+    const tokens = await Moralis.EvmApi.token.getWalletTokenBalances({
+        chain: chain,
+        address: userAddress
+    });
+
+    const nfts = await Moralis.EvmApi.nft.getWalletNFTs({
+        chain: chain,
+        address: userAddress
+    });
+    
+    const myNfts = nfts.raw.result.map((e, i) => {
+        if (e?.media?.media_collection?.high?.url && !e.possible_spam && (e?.media?.category !== "video") ) {
+          return e["media"]["media_collection"]["high"]["url"];
+        }
+    })
+    
+    const balance = await Moralis.EvmApi.balance.getNativeBalance({
+        chain: chain,
+        address: userAddress
+    });
+
+    return res.status(200).json({
+        tokens: tokens.raw,
+        nfts: myNfts,
+        balance: (balance.raw.balance) / (10 ** 18)
+    })
+});
+
 Moralis.start({
     apiKey : process.env.MORALIS_API
 }).then(()=>{
